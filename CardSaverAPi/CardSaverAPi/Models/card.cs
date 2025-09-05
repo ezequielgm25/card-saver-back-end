85c4802a842b85d692d4d8570fed8cc86afa60d3
Ezequiel Garcia Mancebo <ezequielgarcia0925@gmail.com>
2025-09-05 00:17:07 -0400

    first changes in the API creating model for card

---
+++ CardSaverAPi/CardSaverAPi/Models/card.cs
@@ -0,0 +1,11 @@
+﻿namespace CardSaverAPi.Models
+{
+        public class Card
+        {
+            public string Id { get; set; } = Guid.NewGuid().ToString();
+            public string CardNumber { get; set; } = string.Empty;
+            public string ExpirationDate { get; set; } = string.Empty;
+            public string OwnerName { get; set; } = string.Empty;
+            public string CVV { get; set; } = string.Empty;
+        }
+}
