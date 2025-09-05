6e935adb88c5ae5bdee7e4894076b5cd7fa7df97
Ezequiel Garcia Mancebo <ezequielgarcia0925@gmail.com>
2025-09-05 10:21:47 -0400

    Adding core policies

--- CardSaverAPi/CardSaverAPi/Program.cs
+++ CardSaverAPi/CardSaverAPi/Program.cs
@@ -6,6 +6,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Adding  CORS policies 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200") // localhost react applicaton URL
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 
 var app = builder.Build();
 
@@ -15,6 +27,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Use CORS
+app.UseCors("AllowReactApp");
 
 app.UseHttpsRedirection();
 
