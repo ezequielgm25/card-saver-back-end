69c50471cb99ce1504eff65a4ad297d2e4f637c6
Ezequiel Garcia Mancebo <ezequielgarcia0925@gmail.com>
2025-09-07 16:31:48 -0400

    Forgot to push commit with cors policies

--- CardSaverAPi/CardSaverAPi/Program.cs
+++ CardSaverAPi/CardSaverAPi/Program.cs
@@ -6,6 +6,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Adding cors policies
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200") // tu frontend
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -15,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowReactApp");
 
 app.UseHttpsRedirection();
 
